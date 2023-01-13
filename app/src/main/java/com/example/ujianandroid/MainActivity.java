1461fd6900042cf6456df501f095bdf5d55e0142
irnathanasywa <irnathanasywa12@gmail.com>
2023-01-13 08:22:33 +0700

    edit MainActivity.java

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int inputUmur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,28 +33,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String isian_nama_depan = edNamaDepan.getText().toString();
                 String isian_nama_belakang = edNamaBelakang.getText().toString();
-                String isian_umur = edUmur.getText().toString();
+                String isian_nama_umur = edUmur.getText().toString();
 
                 if(isian_nama_depan.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
                 }else{
-                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang);
+                    String nama_lengkap = isian_nama_depan.concat(" ").concat(isian_nama_belakang).concat(", "). concat("usia: " + isian_nama_umur).concat(" kategori: " + inputUmur);
                     daftar_nama.clear();
                     daftar_nama.add(nama_lengkap);
                     edNamaDepan.setText("");
                     edNamaBelakang.setText("");
-                    int umur = Integer.parseInt(((EditText) edUmur).getText().toString());
-                    if(umur <=10) {
-                        Toast.makeText(getApplicationContext(), "Status : Anak", Toast.LENGTH_SHORT).show();
-                    }else if (umur >=10 && umur <=20) {
-                        Toast.makeText(getApplicationContext(), "Status : Remaja", Toast.LENGTH_SHORT).show();
-                    }else if (umur >=20 && umur <=40) {
-                        Toast.makeText(getApplicationContext(), "Status : Dewasa", Toast.LENGTH_SHORT).show();
-                    }else if (umur <40) {
-                        Toast.makeText(getApplicationContext(), "Status : Tua", Toast.LENGTH_SHORT).show();
-                    }
+                    edUmur.setText("");
                     intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
                     startActivity(intent_list);
+                }
+
+                inputUmur = Integer.parseInt(((EditText) edUmur).getText().toString());
+                if (inputUmur <10) {
+                    edUmur.setText("Status:anak");
+                } else if (inputUmur < 20) {
+                    edUmur.setText("Status:remaja");
+                } else if (inputUmur < 40) {
+                    edUmur.setText("Status:dewasa");
+                } else if (inputUmur > 40) {
+                    edUmur.setText("Status:Tua");
                 }
             }
         });
